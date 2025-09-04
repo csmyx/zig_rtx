cef76c3b05c6d35be8336056ba36243216d23d37
Hugo Ma <csmyx@mail.ustc.edu.cn>
2025-09-05 00:41:46 +0800

    feat: add "hittable object" interface and sphere object

---
+++ src/Object.zig
@@ -0,0 +1,84 @@
+const Vec3 = @import("Vec3.zig");
+const Ray = @import("Ray.zig");
+const builtin = @import("builtin");
+const std = @import("std");
+const assert = std.debug.assert;
+
+pub const Object = union(enum) {
+    sphere: Sphere,
+    pub fn hit(o: Object, r: Ray, ray_tmin: f64, ray_tmax: f64) ?HitRecord {
+        return switch (o) {
+            inline else => |o_| o_.hit(r, ray_tmin, ray_tmax),
+        };
+    }
+};
+
+pub const Sphere = struct {
+    center: Vec3,
+    radius: f64,
+    pub fn hit(s: Sphere, r: Ray, ray_tmin: f64, ray_tmax: f64) ?HitRecord {
+        const oc: Vec3 = s.center.subtract(r.ori);
+        const a = r.dir.dot(r.dir);
+        const h = r.dir.dot(oc);
+        const c = oc.dot(oc) - s.radius * s.radius;
+        const discriminant = h * h - a * c;
+        if (discriminant >= 0) {
+            const sqrt_d = @sqrt(discriminant);
+            var t = (h - sqrt_d) / a; // try first root
+            if (!(ray_tmin <= t and t <= ray_tmax)) {
+                t = (h + sqrt_d) / a; // try second root
+                if (!(ray_tmin <= t and t <= ray_tmax)) {
+                    return null;
+                }
+            }
+            const hit_point: Vec3.Point = r.at(t);
+            const outward_normal: Vec3 = hit_point.subtract(s.center).unit();
+            return .init(t, hit_point, r, outward_normal);
+        }
+        return null;
+    }
+};
+
+const HitRecord = struct {
+    t: f64, // hit time
+    p: Vec3.Point, // hit point
+    normal: Vec3, // hit normal vector
+    front_face: bool, // true if hit from outside
+
+    pub fn init(t: f64, p: Vec3.Point, r: Ray, outward_normal: Vec3) HitRecord {
+        if (builtin.mode == .Debug) {
+            // NOTE: the parameter `outward_normal` is assumed to be an unit vector
+            const mag = outward_normal.magnitude();
+            assert(std.math.approxEqAbs(f64, mag, 1, 0.001));
+        }
+        var normal = outward_normal;
+        var front_face = true;
+        if (r.dir.dot(normal) > 0) {
+            normal = normal.opposite();
+            front_face = false;
+        }
+        return .{
+            .t = t,
+            .p = p,
+            .normal = normal,
+            .front_face = front_face,
+        };
+    }
+
+    pub fn equal(self: HitRecord, other: HitRecord) bool {
+        return self.t == other.t and
+            self.front_face == other.front_face and
+            self.p.equal(other.p) and
+            self.normal.equal(other.normal);
+    }
+};
+
+const testing = std.testing;
+
+test "sphere" {
+    const sphere = Object{ .sphere = .{ .center = .init(0, 0, -1), .radius = 0.5 } };
+    const r: Ray = .{ .ori = .init(0, 0, 0), .dir = .init(0, 0, -1) };
+    const hr = sphere.hit(r, 0.0, 1.0);
+    try testing.expect(hr != null);
+    try testing.expect(hr.?.equal(.init(0.5, .init(0, 0, -0.5), r, .init(0, 0, 1))));
+}
--- src/Vec3.zig
+++ src/Vec3.zig
@@ -3,7 +3,7 @@ const Writer = std.Io.Writer;
 
 const Vec3 = @This();
 pub const Point = Vec3;
-pub const Zero: Vec3 = .{ .e = @splat(0) };
+const Zero: Vec3 = .{ .e = @splat(0) };
 
 e: E,
 
@@ -12,42 +12,57 @@ const E = @Vector(3, f64);
 pub fn init(x_: f64, y_: f64, z_: f64) Vec3 {
     return .{ .e = .{ x_, y_, z_ } };
 }
+
 pub fn initE(e: E) Vec3 {
     return .{ .e = e };
 }
+
 pub fn x(self: Vec3) f64 {
     return self.e[0];
 }
+
 pub fn y(self: Vec3) f64 {
     return self.e[1];
 }
+
 pub fn z(self: Vec3) f64 {
     return self.e[2];
 }
+
 pub fn magnitude(self: Vec3) f64 {
     // const sum = v[0] * v[0] + v[1] * v[1] + v[2] * v[2];
     // return std.math.sqrt(sum);
     return @sqrt(@reduce(.Add, self.e * self.e));
 }
+
 pub fn unit(self: Vec3) Vec3 {
     const mag = magnitude(self);
     if (mag == 0) return Zero;
     return self.div(mag);
 }
+
+pub fn opposite(self: Vec3) Vec3 {
+    return self.mul(-1);
+}
+
 pub fn mul(self: Vec3, f: f64) Vec3 {
     const e: E = @splat(f);
     return .{ .e = self.e * e };
 }
+
 pub fn mul3(self: Vec3, other: Vec3) Vec3 {
     return .{ .e = self.e * other.e };
 }
+
 pub fn div(self: Vec3, f: f64) Vec3 {
     const e: E = @splat(f);
     return .{ .e = self.e / e };
 }
+
 pub fn dot(self: Vec3, other: Vec3) f64 {
     return @reduce(.Add, self.e * other.e);
 }
+
 pub fn cross(self: Vec3, other: Vec3) Vec3 {
     return .{ .e = .{
         self.e[1] * other.e[2] - self.e[2] * other.e[1],
@@ -55,11 +70,17 @@ pub fn cross(self: Vec3, other: Vec3) Vec3 {
         self.e[0] * other.e[1] - self.e[1] * other.e[0],
     } };
 }
+
 pub fn add(self: Vec3, other: Vec3) Vec3 {
     return .{ .e = self.e + other.e };
 }
+
 pub fn subtract(self: Vec3, other: Vec3) Vec3 {
     return .{ .e = self.e - other.e };
+}
+
+pub fn equal(self: Vec3, other: Vec3) bool {
+    return @reduce(.And, self.e == other.e);
 }
 
 pub const Fmt = std.fmt.Alt(Vec3, format);
